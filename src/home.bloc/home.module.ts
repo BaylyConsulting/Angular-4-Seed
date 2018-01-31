1fd2dfb9a16bf03afb78ba13c1d0cc7286b77be8
Liam Bayly <ray@baylyconsulting.com>
2018-01-31 09:05:28 -0500

    update test

--- src/home.bloc/home.module.ts
+++ src/home.bloc/home.module.ts
@@ -3,6 +3,8 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
+import { MaterialModule } from '../../src/app/material.module';
+
 import { HomeBlocComponent } from './home.component';
 import { HTTPViewComponent } from './views/http-test/index';
 import { IndexViewComponent } from './views/index/index';
@@ -17,7 +19,7 @@ import { ReleaseService } from './services/release.service';
 import { routing } from './home.routing';
 
 @NgModule({
-  imports: [CommonModule, HttpModule, routing, SharedModule],
+  imports: [CommonModule, HttpModule, routing, SharedModule, MaterialModule],
   declarations: [HomeBlocComponent, HTTPViewComponent, IndexViewComponent, VersionViewComponent],
   providers: [ReleaseService],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
